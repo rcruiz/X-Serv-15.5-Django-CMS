838958da9c984dde993ca67783c3b0a705c8db5a
rcruiz <rc.ruiz@alumnos.urjc.es>
2016-04-05 04:58:14 +0200

    Subiendo

--- cms/views.py
+++ cms/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from models import Pages
 
-# Create your views here.
+
+def mostrar_id(request, page_id):
+    try:
+        contenido = Pages.objects.get(id=int(page_id))
+        return HttpResponse(contenido.name + ' -> ' + contenido.page)
+    except Pages.DoesNotExist:
+        return HttpResponse('Pagina no encontrada')
+
+
+def mostrar_name(request, recurso):
+    try:
+        contenido = Pages.objects.get(name=recurso)
+        return HttpResponse(contenido.page)
+    except Pages.DoesNotExist:
+        return HttpResponse('Pagina no encontrada')
--- myproject/urls.py
+++ myproject/urls.py
@@ -2,9 +2,8 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'myproject.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
 
+    url(r'^(\d+)$', 'cms.views.mostrar_id',),
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^(.*)', 'cms.views.mostrar_name',),
 )
